154c2a1d2c68a4bbcd31d335466821adb96811dd
Kunal07 <50284603+KunalNK@users.noreply.github.com>
2021-05-05 13:41:33 +0530

    Update preq.sh

--- preq.sh
+++ preq.sh
@@ -1,7 +1,5 @@
 curl -O https://bootstrap.pypa.io/get-pip.py
 python3 get-pip.py --user
-pip3 install awscli --upgrade --user
-export PATH=$HOME/.local/bin:$PATH
 sudo pip3 install boto boto3
 sudo yum install python2-pip -y
 sudo pip install boto boto3
